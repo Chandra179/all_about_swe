2588ebf35d772edc79d4f8bd7385c680e1448199
Chandra178 <chandra@bfi.co.id>
2023-09-01 13:08:01 +0700

    add array

--- data_structure/src/Main.java
+++ data_structure/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        System.out.println("==================================================");
+        System.out.println("Exit: 99");
+        System.out.println("==================================================");
+        while (!exit) {
+            System.out.print("Enter i: ");
+            try {
+                int i = scanner.nextInt();
+                if (i == 99) exit = true;
+                Array.execute(i);
+            } catch (Exception e) {
+                throw new RuntimeException("Number only!");
+            }
+        }
+        scanner.close();
     }
 }
